3fd8c7f21a7f7a208fd23fd43c7034a666272760
StuartC63 <stuart_cherrington@hotmail.co.uk>
2025-11-06 11:04:27 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_04_e12d3ad3a488-22531-20869"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
